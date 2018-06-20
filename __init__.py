9d3f4362b6975d436604527d832e318075526f6a
Maura Rowell <Maura.Rowell@MountainPacificGroup.com>
2018-06-20 12:10:12 -0700

    intial commit

---
+++ __init__.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+'''
+.. module:: plinky
+    :language: Python Version 2.7
+    :platform: Windows 10
+    :synopsis: manage SSH nonnections with Python
+
+.. moduleauthor:: Jason Thorpe and Maura Rowell <mkrowell@uw.edu>
+'''
+
+# usage:
+#-- connection_test(1722)
+#-- start_plink('MPG-AWS-Cloud1')
+#-- connection_test(1722)
+#-- stop_plink()
+#-- connection_test(1722)
+# --------------------------------------------------------------------------------
+
+from subprocess import  Popen, PIPE
+from socket import socket,AF_INET,SOCK_STREAM
+
+__plink__ = None
+
+def test_connection(port, host = "127.0.0.1"):
+    s = socket(AF_INET, SOCK_STREAM)
+    try:
+        s.connect((host,port))
+        return True
+    except:
+        return False
+    finally:
+        s.close()
+
+def start(profile):
+    global __plink__
+    if __plink__ is not None:
+        if __plink__.poll() is None:
+            # plink is already running
+            return
+        else:
+            __plink__ = None
+    try:
+        p = Popen(['plink.exe','-N','-load',profile],stdin = PIPE)
+    except OSError as err:
+        msg =  "Failed to start plink.  Please make sure plink.exe is on the system path."
+        print msg
+        err.message =  msg + "\n" + err.message
+        p.kill()
+        raise err
+    except Exception as err:
+        print "plink failed to start"
+        p.kill()
+        raise err
+    __plink__ = p
+
+
+def stop():
+    global __plink__
+    if __plink__ is not None:
+        if __plink__.poll() is None:
+            # plink is running
+            __plink__.kill()
+        __plink__ = None
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Usage:
+
+After setting up a PuTTY saved session (e.g., foo), plinky can be used as follows:
+
+```python
+	import plinky
+
+	# connect to a remote server using the PuTTY session named "foo"
+	plinky.start("foo")
+
+	# test whether there is a process listening on local port 1722
+	plinky.test_connection(1722)
+
+	# test whether there is a process listening on local port 1722
+	plinky.stop()
+```
+
+# Details
+* **plinky.start**(*profile*) Opens a connection to the remote server with the saved
+	session profile *"profile"*. An exception is raised if the process fails to
+	start, but not necessarily if the connection fails.
+
+* **plinky.test_connection**(*port, host = "127.0.0.1"*) Tests if a connection
+	exists at the specified `port` and `host`. The host defaults to [localhost].
+
+* **plinky.stop**() Closes the current plink session by calling the
+	`kill()` method of the subprocess in which plink is running.
+	Note that calling `plinky.stop()` does not cause the connection to be
+	closed immediately, and as such, the call to `test_connection()` in the
+	following snippet may return `True`:
+
+```python
+	import plinky
+	plinky.start("foo")
+	plinky.stop()
+	plinky.test_connection()
+```
+
+# Quirks
+
+For now, plinky only supports a single SSH session; calling
+'plinky.start("foo"); plinky.start("bar");' will cause the SSH session to
+"foo" to be closed before the connection with "bar" is opened.
